1aa3f3c9d652730ed1d536ed14140dd9fcd91109
Maria T. Andersen <mteilmann93@gmail.com>
2022-10-15 08:37:27 +0200

    Implement a click handler for when an element within the seats class is clicked. Test if the event handler works.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 const availableSeats = ["1", "2", "3"];
 const takenSeats = [];
+
+const seats = document.querySelectorAll(".seats");
+
+seats.forEach((seat) => {
+  seat.addEventListener("click", function handleClick() {
+    console.log("seat clicked");
+
+    seat.setAttribute("style", "background-color: red;");
+  });
+});
