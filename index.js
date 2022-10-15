9b6c0c5565fa620e6ceea43cfae8bd10b455da42
Maria T. Andersen <mteilmann93@gmail.com>
2022-10-15 11:15:34 +0200

    Restructure code so instead of having two arrays that store available or taken seats, we have one array that contains both. Adapt the code according to this change. Also, if isBooked equals true, add a class 'isBooked' to the clicked element.

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
-const availableSeats = [
+const allSeats = [
   { id: "1", isBooked: false },
   { id: "2", isBooked: false },
   { id: "3", isBooked: false },
 ];
-const takenSeats = [];
 
 const seats = document.querySelectorAll(".seats");
 
 seats.forEach((seat) => {
   seat.addEventListener("click", function handleClick() {
-    const currentIndex = availableSeats.findIndex(
+    const currentIndex = allSeats.findIndex(
       (availableSeat) => availableSeat.id === seat.id
     );
 
-    availableSeats[currentIndex].isBooked =
-      !availableSeats[currentIndex].isBooked;
+    allSeats[currentIndex].isBooked = !allSeats[currentIndex].isBooked;
 
-    const bookedSeat = availableSeats.splice(currentIndex, 1);
-    takenSeats.push(bookedSeat[0]);
+    if (allSeats[currentIndex].isBooked) {
+      seat.classList.add("isBooked");
+    } else {
+      seat.classList.remove("isBooked");
+    }
 
-    seat.setAttribute("style", "background-color: red;");
-
-    console.log("availableSeats: ", availableSeats);
-    console.log("takenSeats: ", takenSeats);
+    console.log("allSeats: ", allSeats);
   });
 });
