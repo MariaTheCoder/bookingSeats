c7e8257a5633ef0dc0ae3fb3da691a74dd06adf5
Maria T. Andersen <mteilmann93@gmail.com>
2022-10-15 08:58:08 +0200

    Restructure the content of the availableSeats array so each seat is an object instead of a string. Each seat should have a isBooked property which is initially is set to false. Upon click, change this property to true and remove the object from the avaiableSeats array and push it into the takenSeats array instead.

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-const availableSeats = ["1", "2", "3"];
+const availableSeats = [
+  { id: "1", isBooked: false },
+  { id: "2", isBooked: false },
+  { id: "3", isBooked: false },
+];
 const takenSeats = [];
 
 const seats = document.querySelectorAll(".seats");
 
 seats.forEach((seat) => {
   seat.addEventListener("click", function handleClick() {
-    console.log("seat clicked");
+    seat.isBooked = true;
+
+    const currentIndex = availableSeats.findIndex(
+      (availableSeat) => availableSeat.id === seat.id
+    );
+
+    const bookedSeat = availableSeats.splice(currentIndex, 1);
+    takenSeats.push(bookedSeat[0]);
 
     seat.setAttribute("style", "background-color: red;");
+
+    console.log("availableSeats: ", availableSeats);
+    console.log("takenSeats: ", takenSeats);
   });
 });
